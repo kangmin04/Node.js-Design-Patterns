68bce0ff1efb5d81ff4d782e30c46b483bb6f5db
김강민 <>
2025-12-26 17:11:57 +0900

    Feat. Lazy Promise understanding

--- chapters/chapter5_promiseAsync/promise/lazyPromise/lazypromise_class.js
+++ chapters/chapter5_promiseAsync/promise/lazyPromise/lazypromise_class.js
@@ -1,5 +1,5 @@
 export class LazyPromise extends Promise { // 1
-    #resolve // 2
+    #resolve //private member
     #reject      
     #executor
     #promise
@@ -13,7 +13,7 @@ export class LazyPromise extends Promise { // 1
       //super 호출이 끝나야만, this를 사용할 수 있다. !! 
       this.#resolve = _resolve
       this.#reject = _reject
-      this.#executor = executor
+      this.#executor = executor 
       this.#promise = null
     }
     #ensureInit() { // 4
@@ -38,3 +38,27 @@ export class LazyPromise extends Promise { // 1
       return this.#promise.finally(onFinally)
     }
   }
+
+  // const lazyPromise = new LazyPromise(resolve => { // 1
+  //   console.log('Executor Started!')
+  //   // simulate some async work to be done
+  //   setTimeout(() => {
+  //     resolve('Completed!')
+  //   }, 1000)
+  // })
+  // console.log('Lazy Promise instance created!')
+  // console.log(lazyPromise)
+  // lazyPromise.then(value => { // then is invoking executor function  !! 
+  //   console.log(value)
+  //   console.log(lazyPromise)
+  // })
+
+  const normalPromise = new Promise((resolve, reject) => {
+    console.log('Executor Started!')
+      // simulate some async work to be done
+      setTimeout(() => {
+        resolve('Completed!')
+      }, 1000)
+  })
+    console.log('Promise instance created!')
+  console.log(normalPromise)
