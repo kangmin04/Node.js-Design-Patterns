c1c0b2e469b62ba8729eb5b4e7ffba69ce9abe4e
kangmin040823 <146157505+kangmin04@users.noreply.github.com>
2026-03-03 15:43:07 +0000

    Feat. implement parametrized test and suite(describe, it) allias

---
+++ chapters/chapter10_test/05-suite-test/suite.test.mjs
@@ -0,0 +1,16 @@
+import {test, suite, describe, it} from 'node:test'
+
+suite('Top level suite' , {concurrency: true} , () => {
+    test('Subtest 1', () => {})
+    test('Subtest 2', () => {})
+})
+
+/*
+    Suite는 관련된 테스트들을 하나의 그룹으로 묶어 구조화하는 논리적인 단위
+    suite = describe, test= it (다른 언어, 프레임워크에서 주로 사용되는 방식)
+*/
+
+describe('Top level suite' , {concurrency: true} , () => {
+    it('Subtest 1', () => {})
+    it('Subtest 2', () => {})
+})
