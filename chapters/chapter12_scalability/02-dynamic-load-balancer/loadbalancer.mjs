e693caf63da1d29bac62f69e2dacc477bc952003
kangmin040823 <146157505+kangmin04@users.noreply.github.com>
2026-04-16 07:39:43 +0000

    Feat. implement custom load-balancer using proxy and round robin algorithm. Test consul service registry

---
+++ chapters/chapter12_scalability/02-dynamic-load-balancer/loadbalancer.mjs
@@ -0,0 +1,44 @@
+import { createServer } from "node:http";
+import {createProxyServer} from 'httpxy'
+import {ConsulClient} from './consul.mjs'
+
+const routing = [
+    {
+        path: '/api', 
+        service: 'api-service', 
+        index: 0
+    },
+    {
+        path: '/', 
+        service: 'webapp-service', 
+        index: 0 // round robin을 위한, last server 기록용 인덱스 
+    }
+] 
+const consulClient = new ConsulClient(); 
+const proxy =createProxyServer(); 
+
+const server = createServer(async (req, res) => {
+    const route = routing.find(route => req.url.startsWith(route.path))
+    try{
+        const services = await consulClient.getAllServices(); 
+        const servers = Object.values(services).filter(service => service.Tags.includes(route.service)) 
+        if(servers.length > 0){
+            route.index = (route.index + 1) % servers.length; 
+            const server = servers[route.index]; 
+            const target = `http://${server.Address}:${server.Port}`
+            proxy.web(req, res, {target})
+            return; 
+        }
+    }catch(err){
+        console.error(err); 
+    }
+    //service not found.
+    res.writeHead(502); 
+    return res.end('Bad gateway')
+})
+
+server.listen(8080, () => {
+    console.log(`Load balancer started on port 8080`)
+})
+
+
