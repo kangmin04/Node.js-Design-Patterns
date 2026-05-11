a8505bfe4e224f86496170cf774ac948d699a8a2
kangmin040823 <146157505+kangmin04@users.noreply.github.com>
2026-05-11 14:54:38 +0000

    Feat. implement redis stream for chatapp. And strive to implement different message box color and positon by whom sent the message

--- chapters/chapter13_messaging-integration/05-chat-redisStream/index.mjs
+++ chapters/chapter13_messaging-integration/05-chat-redisStream/index.mjs
@@ -1,3 +1,10 @@
+/*
+    redisClient -----------  centralized in memory server  ------------- redisClientXread
+                xadd()로 field 저장                          xread()로 읽어옴. (이때 하나 읽어올 때 다른 작업은 못함! 싱글스레드)
+                (key-value)                                 연결 끊긴 후에도 마지막 record를 기억하기에 해당 record 이후부터 읽어오면 됨
+                client와 서버는 tcp/ip연결 
+*/
+
 import { createServer } from "node:http";
 import staticHandler from 'serve-handler'
 import { WebSocketServer } from "ws";
@@ -9,7 +16,6 @@ const server = createServer((req, res) => {
     return staticHandler(req, res, { public: 'web' })
   })
   
-
 const wss = new WebSocketServer({server}); 
 wss.on('connection', async client => {
     console.log('client connected'); 
@@ -24,9 +30,18 @@ wss.on('connection', async client => {
             })
         )
     } )
-
+    /* client 에서 connection할 때마다(웹 새로고침할 때마다) 전체 chat data 가져옴 */
     const logs = await redisClient.xrange('chat_stream', '-', '+')
-    for (const [,[, message]] of logs){
+    console.log(`chat logs: ${logs}`)
+    /* 
+        for (const [recordId, [propertyId, message]] of logs 을 destructuring함) 
+        logs엔 [
+            ["158859110918-0", ["message", "this is a new message"]], 
+            ["158859110918-1", ["message", "this is another message"]], 
+        ] 다음과 같은 형식임! 
+
+    */   
+    for (const [,[, message]] of logs){    
         client.send(Buffer.from(message))
     }
 })
@@ -43,14 +58,19 @@ let lastRecordId = '$'
 async function processStreamMessages(){
     while(true){
         const [[, records ]] = await redisClientXread.xread(
-            'BLOCK', '0', 
-            'STREAMS',
-            'chat_stream', 
-            lastRecordId
+            'BLOCK', '0', /* wait forever till new msg arrive */
+            'STREAMS', /* keyword that we r now going to specify the details of the streams */ 
+            'chat_stream', /* name of the stream we want to read */
+            lastRecordId /* $: 가장 처음부터 읽는 sign.  */
         )
 
         for(const [recordId, [, message]] of records){
+            console.log(`Message from stream: ${message}`)
             broadcast(Buffer.from(message))
+            lastRecordId = recordId
         }
     }
 }
+
+processStreamMessages().catch(err => console.log(err))
+server.listen(process.argv[2] || 8080 , () => console.log(`server is runnning on http://localhost:${process.argv[2]} `))
