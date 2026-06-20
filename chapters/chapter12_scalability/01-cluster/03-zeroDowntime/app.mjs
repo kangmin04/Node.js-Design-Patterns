4b55d37c89f690301b9d5abcbe2215a7cc287df8
kangmin04 <146157505+kangmin04@users.noreply.github.com>
2026-06-20 22:29:36 +0900

    Feat. review chapter 12_scalability-01_cluster and configure docker, nginx

--- chapters/chapter12_scalability/01-cluster/03-zeroDowntime/app.mjs
+++ chapters/chapter12_scalability/01-cluster/03-zeroDowntime/app.mjs
@@ -4,7 +4,8 @@ import cluster from "node:cluster";
 import { once } from "node:events";
 
 if(cluster.isPrimary){
-    const availableCpus = cpus(); 
+    const availableCpus = cpus();
+    // const mockCpu = [1, 2]; 
     console.log(`Primary ${process.pid} is running`)
     for(const _ of availableCpus){
         cluster.fork()
@@ -19,23 +20,34 @@ if(cluster.isPrimary){
 
     process.on("SIGUSR2", async () => { /* SIGUSR : User가 정의하는 이벤트 KILL SIGUSR2 PID */
         const workers = Object.values(cluster.workers); 
+        // console.log('[DEBUG] workers: ', workers)
         for (const worker of workers){
             console.log(`Stopping worker: ${worker.process.pid}`)
+            //1. 안전한 종료 시도
             worker.disconnect(); /* graceful stop. 새 연결 거부하고, 기존 작업(진행중이던) 완료하며, 연결한 모든 클라이언트와의 접속 끊어지길 기다림. 이후 스스로 정상종료 후 종료되는 시점에 primary process는 cluster은 해당 워커에 대한 exit 이벤트르 발행함 */
+            
+            // 2. 2초 뒤에도 안 죽으면 강제 종료시키는 타이머 설정 (처음 코드에선 setInterval 작업이 계속해서 worker process에서 돌아갔기에 기존 작업이 종료가 안되서, 새로운 fork가 안일어났음. 
+            //다만 실무 환경에선 백그라운드에서 계속 setINterval등이 돌아가는 경우도 있을수 있음(db) 그렇기에 관리자처럼 timeout 기능을 도입하는게 적합! )
+            const killTimeout = setTimeout(() => {
+                if (!worker.isDead()) {
+                    console.log(`Worker ${worker.process.pid} 미종료. 강제 종료(kill) 진행`);
+                    worker.kill();
+                }
+            }, 2000);
             await once(worker, 'exit'); 
-            if(!worker.exitedAfterDisconnect) continue
+            if(!worker.exitedAfterDisconnect) continue /* 의도된 disconnect 시 대체 worker fork.  */
             const newWorker = cluster.fork(); 
             await once(newWorker, 'listening')
         }
     })
 }else{ /* 각 워커는 different Nodejs process with its own event loop, memory space, and loaded moduels */
 
-    setInterval(
-        () => {
-            if(Math.random() < 0.2){
-                throw new Error(`Ops.. ${process.pid} crashed`)
-            }
-        }, Math.ceil(Math.random() * 3) * 1000)
+    // setInterval(
+    //     () => { /* 무중단 배포 로직에 집중하고자, ERROR 야기 코드 없앰. -> Primary Process 멈추면 자동으로 worker들이 하나씩 생성.  */
+    //         // if(Math.random() < 0.2){
+    //         //     throw new Error(`Ops.. ${process.pid} crashed`)
+    //         // }
+    //     }, Math.ceil(Math.random() * 8) * 1000)
     const server = createServer((_req, res) => {
         /* Simulate CPU intensive work */
         let i = 1e7; 
