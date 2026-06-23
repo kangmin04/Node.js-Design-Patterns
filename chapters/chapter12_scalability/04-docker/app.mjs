8385864ff9a9f6e16c1307e1c6ea1a50abba9890
kangmin04 <146157505+kangmin04@users.noreply.github.com>
2026-06-23 17:50:59 +0900

    Feat. study on k8s and implement basic command for creating cluster, getting pods, making services and implementing rollout deployment

--- chapters/chapter12_scalability/04-docker/app.mjs
+++ chapters/chapter12_scalability/04-docker/app.mjs
@@ -1,7 +1,9 @@
 import { createServer } from "node:http";
 import { hostname } from "node:os";
 
-const version = 1; 
+/* rollout test: prev는 1이었고, 무중단 배포 테스트!  */
+const version = 2;
+
 
 createServer((_req, res) => {
     res.end(`Hello from ${hostname()} (v${version})`)
